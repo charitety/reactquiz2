2d6f2af1e426cc6a7e09234b5cfe75244d501518
Caridad RoblesCarvajal <croblescarvajal@xpanxion.com>
2023-05-30 10:49:17 -0400

    submit

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
 export const NumberBuilder = () => {
+    let[listOfNumbers, setListOfNumbers] = useState("");
+    let[counter, setCounter] = useState(0);
 
+
+    const numberTracker = (n) => {
+        if (n=== "add"){
+            setCounter(++counter);
+            setListOfNumbers ((listOfNumbers += counter.toString()+""));
+        }
+    }else{
+        setCounter(0);
+        setListOfNumbers("");
+    }
+
+    
     return (
-        <>
-            <button>Add Number</button>
-            <button>Reset</button>
-            <br></br>
-            Numbers: 
-        </>
+        <div>
+            <button onClick={()=> numberTracker("add")}>Add Number</button>
+            <button onClick={()=> numberTracker("reset")}>Reset</button>
+            <br>
+            Numbers: {listOfNumbers}
+            </br>
+           
+        </div>
     )
-}
+};
